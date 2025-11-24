01ec8602542cb803607b3eb2a6c0863ebaf60b8d
称霸幼儿园 <yxlmzch@163.com>
2025-11-24 13:29:56 +0800

    update: es url从配置文件获取

--- anynote-common/anynote-common-elasticsearch/src/main/java/com/anynote/common/elasticsearch/config/ElasticsearchConfig.java
+++ anynote-common/anynote-common-elasticsearch/src/main/java/com/anynote/common/elasticsearch/config/ElasticsearchConfig.java
@@ -6,6 +6,7 @@ import co.elastic.clients.transport.ElasticsearchTransport;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,9 +16,12 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ElasticsearchConfig {
 
+    @Value("${elasticsearch.url}")
+    private String elasticsearchUrl;
+
     @Bean
     public ElasticsearchClient client() {
-        RestClient restClient = RestClient.builder(HttpHost.create("http://localhost:9200"))
+        RestClient restClient = RestClient.builder(HttpHost.create(elasticsearchUrl))
                 .build();
         ElasticsearchTransport transport = new RestClientTransport(
                 restClient, new JacksonJsonpMapper());
